d23148a7ea8053d3b8c2d56564bd0c4f85b596b4
Cetijunior <lameceti1@gmail.com>
2023-12-01 20:56:42 +0100

    first commit

---
+++ src/components/Playlists.jsx
@@ -0,0 +1,98 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { reducerCases } from "../utils/Constants";
+import { useStateProvider } from "../utils/StateProvider";
+
+export default function Playlists() {
+  const [{ token, playlists }, dispatch] = useStateProvider();
+  const [demoAppError, setDemoAppError] = React.useState(false);
+
+  useEffect(() => {
+    const getPlaylistData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.spotify.com/v1/me/playlists",
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const { items } = response.data;
+        const playlists = items.map(({ name, id }) => {
+          return { name, id };
+        });
+        dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
+      } catch (error) {
+        if (error.response && error.response.status === 403) {
+          // Handle 403 error (Forbidden)
+          setDemoAppError(true);
+          alert("This is a demo app. Some features may be restricted.");
+        } else {
+          // Handle other errors
+          console.error("Error fetching playlists:", error.message);
+        }
+      }
+    };
+
+    getPlaylistData();
+  }, [token, dispatch]);
+
+  const changeCurrentPlaylist = (selectedPlaylistId) => {
+    dispatch({ type: reducerCases.SET_PLAYLIST_ID, selectedPlaylistId });
+  };
+
+  return (
+    <Container>
+      {demoAppError && <div className="demo-warning">Demo App Warning</div>}
+      <ul>
+        {playlists.map(({ name, id }) => {
+          return (
+            <li key={id} onClick={() => changeCurrentPlaylist(id)}>
+              {name}
+            </li>
+          );
+        })}
+      </ul>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  color: #b3b3b3;
+  height: 100%;
+  overflow: hidden;
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    height: 55vh;
+    max-height: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.7rem;
+      &-thumb {
+        background-color: rgba(255, 255, 255, 0.6);
+      }
+    }
+    li {
+      transition: 0.3s ease-in-out;
+      cursor: pointer;
+      &:hover {
+        color: white;
+      }
+    }
+  }
+
+  .demo-warning {
+    background-color: #ffcc00;
+    color: #333;
+    padding: 1rem;
+    text-align: center;
+    font-weight: bold;
+  }
+`;
